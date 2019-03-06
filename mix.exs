38be03d770ccd7ecef264a6bf81818425abbee98
M. Simon Borg <msimonborg@gmail.com>
2019-03-06 09:34:00 -0500

    use coveralls to measure test coverage

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Receiver.MixProject do
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       description: description(),
       package: package(),
       source_url: "https://github.com/msimonborg/receiver",
@@ -38,7 +40,8 @@ defmodule Receiver.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.19.3", only: :dev}
+      {:ex_doc, "~> 0.19.3", only: [:dev, :test]},
+      {:excoveralls, "~> 0.10", only: :test},
     ]
   end
 end
