c13fb3c9c7a4ba23d3e794c6013f54c8f8ed2c79
M. Simon Borg <msimonborg@gmail.com>
2019-03-10 12:56:02 -0400

    only run mix tasks in test env

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -1,15 +1,17 @@
-defmodule Mix.Tasks.Receiver.Build do
-  @moduledoc false
-  alias Mix.Tasks.{Coveralls, Credo, Docs, Inch}
+if Mix.env() == :test do
+  defmodule Mix.Tasks.Receiver.Build do
+    @moduledoc false
+    alias Mix.Tasks.{Coveralls, Credo, Docs, Inch}
 
-  use Mix.Task
+    use Mix.Task
 
-  @preferred_cli_env :test
+    @preferred_cli_env :test
 
-  def run([]) do
-    Coveralls.Html.run([])
-    Credo.run(["--strict"])
-    Inch.run([])
-    Docs.run([])
+    def run([]) do
+      Coveralls.Html.run([])
+      Credo.run(["--strict"])
+      Inch.run([])
+      Docs.run([])
+    end
   end
 end
--- lib/mix/tasks/safe_travis.ex
+++ lib/mix/tasks/safe_travis.ex
@@ -1,16 +1,18 @@
-defmodule Mix.Tasks.Coveralls.SafeTravis do
-  @moduledoc false
-  alias Mix.Tasks.Coveralls
+if Mix.env() == :test do
+  defmodule Mix.Tasks.Coveralls.SafeTravis do
+    @moduledoc false
+    alias Mix.Tasks.Coveralls
 
-  use Mix.Task
+    use Mix.Task
 
-  @preferred_cli_env :test
-  @shortdoc "A safe `coveralls.travis` variant that doesn't crash on failed upload."
+    @preferred_cli_env :test
+    @shortdoc "A safe `coveralls.travis` variant that doesn't crash on failed upload."
 
-  def run(args) do
-    Coveralls.Travis.run(args)
-  rescue
-    e in ExCoveralls.ReportUploadError ->
-      Mix.shell().error("Failed coveralls upload: #{Exception.message(e)}")
+    def run(args) do
+      Coveralls.Travis.run(args)
+    rescue
+      e in ExCoveralls.ReportUploadError ->
+        Mix.shell().error("Failed coveralls upload: #{Exception.message(e)}")
+    end
   end
 end
