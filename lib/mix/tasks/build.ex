483db424f9409c3d196847f0492aa9701ff21d75
M. Simon Borg <msimonborg@gmail.com>
2019-03-10 15:03:20 -0400

    enforce formatting and require Elixir >= 1.8 for development

--- lib/mix/tasks/build.ex
+++ lib/mix/tasks/build.ex
@@ -7,20 +7,37 @@ if Mix.env() == :test do
     use Mix.Task
 
     @preferred_cli_env :test
+    @required_elixir_version "1.8"
 
-    @spec run([]) :: [any()]
-    def run([]) do
-      IO.puts("#{cyan()}#{bright()}Running formatter")
-      do_run([])
+    @spec run(argv :: [String.t()]) :: nil
+    def run(argv) do
+      {opts, argv, _} = OptionParser.parse(argv, switches: [format: :boolean])
+      if Keyword.get(opts, :format, true), do: run_formatter(argv)
+      do_run(argv)
     end
 
-    @spec do_run([binary()]) :: [any()]
+    @spec run_formatter([binary()]) :: any()
+    def run_formatter(argv) do
+      if System.version() >= @required_elixir_version do
+        IO.puts("#{cyan()}#{bright()}Running formatter")
+        Format.run(["--check-equivalent" | argv])
+      else
+        raise RuntimeError, """
+        #{bright()}Elixir version must be >= #{@required_elixir_version}. Detected version:
+
+          * #{System.version()}
+
+        Please upgrade to Elixir #{@required_elixir_version} or above to continue development on this project.
+        """
+      end
+    end
+
+    @spec do_run([binary()]) :: nil
     def do_run(argv) do
-      Format.run(["--check-equivalent"])
       Coveralls.Html.run(argv)
-      Credo.run(["--strict"])
       Inch.run(argv)
       Docs.run(argv)
+      Credo.run(["--strict" | argv])
     end
   end
 end
--- test/mix/tasks_test.exs
+++ test/mix/tasks_test.exs
@@ -2,21 +2,19 @@ defmodule ExUnit.Mix.Tasks.Receiver.BuildTest do
   use ExUnit.Case
   import Mock
   alias Mix.Tasks.{Coveralls, Coveralls.SafeTravis, Coveralls.Travis}
-  alias Mix.Tasks.{Credo, Docs, Format, Inch, Receiver.Build}
+  alias Mix.Tasks.{Credo, Docs, Inch, Receiver.Build}
 
   test "runs build task" do
     with_mocks([
       {Coveralls.Html, [], [run: fn _ -> nil end]},
       {Credo, [], [run: fn _ -> nil end]},
       {Docs, [], [run: fn _ -> nil end]},
-      {Format, [], [run: fn _ -> nil end]},
       {Inch, [], [run: fn _ -> nil end]}
     ]) do
-      Build.do_run([])
+      Build.run(["--no-format"])
       assert_called(Coveralls.Html.run([]))
       assert_called(Credo.run(["--strict"]))
       assert_called(Docs.run([]))
-      assert_called(Format.run(["--check-equivalent"]))
       assert_called(Inch.run([]))
     end
   end
