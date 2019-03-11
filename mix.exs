df493a7a647c8df241f803406817abae808bf5d1
M. Simon Borg <msimonborg@gmail.com>
2019-03-10 20:09:40 -0400

    bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Receiver.MixProject do
   def project do
     [
       app: :receiver,
-      version: "0.1.7",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
